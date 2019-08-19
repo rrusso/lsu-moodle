7a45429320e48a457b13506d001a34db76251a29
Robert Russo <rrusso@lsu.edu>
2019-08-19 07:48:27 -0500

    Because LSU's LDAPS is unreliable, bypass it for existing users.

--- auth/cas/auth.php
+++ auth/cas/auth.php
@@ -299,7 +299,14 @@ class auth_plugin_cas extends auth_plugin_ldap {
         if (empty($this->config->host_url)) {
             return array();
         }
-        return parent::get_userinfo($username);
+        // BEGIN LSU ldaps bypass if user exists.
+        global $DB;
+        $conditions = array('username' => $username);
+        $user = $DB->get_record('user', $conditions);
+        if (!isset($user->firstname)) {
+            return parent::get_userinfo($username);
+        }
+        // END LSU ldaps bypass if user exists.
     }
 
     /**
